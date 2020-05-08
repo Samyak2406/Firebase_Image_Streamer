2a50a778f483ace209a221cba9c4871347c435e6
1234 <samyak.vce18@sot.pdpu.ac.in>
2020-05-08 15:30:08 +0530

    Removed unnescessary dependencies . Made it compeletely internet dependent

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:firebase/firebase.dart';
 import 'package:flutter/material.dart';
 import 'package:imagesonline/finalScreen.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'my_storage.dart';
 
-//final _store=Firestore.instance;
+
+final _store=Firestore.instance;
 
 void main() => runApp(MyApp());
 
@@ -17,17 +18,18 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-//    makeMyList();
+    makeMyList();
   }
 
-//  void makeMyList() async{
-//    await for(var pokemon in _store.collection('pokemon').snapshots()){
-//      for(var pokemonName in pokemon.documents){
-//        data.add(pokemonName.toString());
-//        print(pokemonName);
-//      }
-//    }
-//  }
+  void makeMyList() async{
+    await for(var pokemon in _store.collection('pokemon').snapshots()){
+      for(var pokemonName in pokemon.documents){
+        data.add(pokemonName.data['name'].toString());
+        print(pokemonName.data['name'].toString());
+//        print('find me');
+      }
+    }
+  }
 
 
   @override
